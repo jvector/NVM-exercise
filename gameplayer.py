2d738dbc4ecf7eb79ca3ca6501bf46ae73c1cf7c
Victor Churchill <victor.churchill@godaddy.com>
2017-11-02 13:38:37 +0000

    obey(commandstring)

--- gameplayer.py
+++ gameplayer.py
@@ -1,17 +1,14 @@
 """
-Variables accessed by the functions...
-
+Variables accessed and modified by the functions...
 direction: 0..3 for NESW
 xpos: 0..4
 ypos: 0..4
 """
-
 direction = 0
 xpos = 0
 ypos = 0
 
 debug = 1
-history = ''
 
 def move():
     '''
@@ -31,12 +28,8 @@ def move():
     (xpos,ypos) = (max(xpos, 0), max(ypos,0))
 
     if debug:
-        print 'Move! : ' + getstate()
+        print 'Move : ' + getstate()
 
-def getstate():
-    return "{0} {1} {2}".format(xpos, ypos, { 0:'N', 1:'E', 2:'S', 3:'W' }[direction])
-
-    
 def turnleft():
     '''
     Turn anticlockwise by decrementing the direction integer. wrap back to +3 when we go from N to W.
@@ -46,19 +39,46 @@ def turnleft():
     direction = dmap.get(direction, direction)
 
     if debug:
-        print 'Left! : ' + getstate()
+        print 'Left : ' + getstate()
 
 def turnright():
     '''
-    Turn anticlockwise by decrementing the direction integer. wrap back to +3 when we go from N to W.
+    Turn clockwise by incrementing the direction integer. wrap back to 0 when we go from W to N.
     '''
     global direction
-    dmap = {0:3, 1:0, 2:1, 3:2}
+    dmap = {0:1, 1:2, 2:3, 3:0}
     direction = dmap.get(direction, direction)
 
     if debug:
-        print 'Right! : ' + getstate()
+        print 'Right : ' + getstate()
 
+def getstate():
+    return "{0} {1} {2}".format(xpos, ypos, { 0:'N', 1:'E', 2:'S', 3:'W' }[direction])
 
+'''
+Main control function for the player. Input a string of [M,R,L] characters causing the token
+to move, turn left and turn right respectively.
+'''
+
+def obey(commandstring):
+    # Loop over each character in commandstring.
+    for index in range(0, len(commandstring)):
+
+        # Get one-character substring and execute. PO says no validation required. 
+        char = commandstring[index]
+        if char == "M":
+            move();
+        elif char == "R":
+            turnright();
+        elif char == "L":
+            turnleft()
+
+def _RESET():
+    global xpos, ypos, direction
+    (xpos, ypos, direction) = (0,0,0)
+    if debug:
+        print 'RESET : ' + getstate()
+
+#end.
 
 
